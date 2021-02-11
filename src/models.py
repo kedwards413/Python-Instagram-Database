c38487f2247c2f370e1fd374ec34bbffe8f2cd1c
kedwards413 <kaela.rose.edwards@gmail.com>
2021-02-11 01:29:14 +0000

    final update

--- src/models.py
+++ src/models.py
@@ -9,14 +9,14 @@ from eralchemy import render_er
 Base = declarative_base()
 
 #class Person(Base):
-    __tablename__ = 'person'
+    #__tablename__ = 'person'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     #id = Column(Integer, primary_key=True)
     #name = Column(String(250), nullable=False)
 
 #class Address(Base):
-    __tablename__ = 'address'
+    #__tablename__ = 'address'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     #id = Column(Integer, primary_key=True)
@@ -36,6 +36,35 @@ class User(Base):
     firstname = Column(String(250), nullable=False)
     lastname = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False)
+
+class Follower(Base): #bridge table
+    __tablename__ = 'follower'
+    id = Column(Integer, primary_key=True)
+    user_from_id = Column(Integer, ForeignKey('user.id'))
+    user_to_id = Column(Integer, ForeignKey('user.id'))
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    media_type = Column(String(250), nullable=False)
+    url = Column(String(250), nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(500), nullable=False)
+    author_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+
+
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
