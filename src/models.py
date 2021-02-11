316fcbe97410111ef6417e78914d31e5a1731388
kedwards413 <kaela.rose.edwards@gmail.com>
2021-02-11 00:48:33 +0000

    initial commit

--- src/models.py
+++ src/models.py
@@ -8,26 +8,34 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
+#class Person(Base):
     __tablename__ = 'person'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    #id = Column(Integer, primary_key=True)
+    #name = Column(String(250), nullable=False)
 
-class Address(Base):
+#class Address(Base):
     __tablename__ = 'address'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    #id = Column(Integer, primary_key=True)
+    #street_name = Column(String(250))
+    #street_number = Column(String(250))
+    #post_code = Column(String(250), nullable=False)
+    #person_id = Column(Integer, ForeignKey('person.id'))
+    #person = relationship(Person)
 
-    def to_dict(self):
-        return {}
+    #def to_dict(self):
+        #return {}
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250), nullable=False)
+    firstname = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
